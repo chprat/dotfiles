945bd3de1a174b06ada697c371ec852541b7e14f
chprat <49040338+chprat@users.noreply.github.com>
2023-09-15 00:40:37 +0200

    setup: Add user to dialout group

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+# add user to dialout
+if ! groups | grep -q dialout; then
+    sudo usermod -aG dialout "$USERNAME"
+fi
+
 # create ~/.local/bin
 [ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"

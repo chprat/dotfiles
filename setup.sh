26d2e55b1e2b6b5df07259f523b5ff06a9e427fc
chprat <49040338+chprat@users.noreply.github.com>
2023-09-15 00:40:37 +0200

    setup: Install delta

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 dir_name=$(dirname "$0")
 code_path=$(realpath "$dir_name")
 
+delta_release="0.16.5"
+
 # install packages
 xargs sudo apt install < "$code_path/packages"
 
@@ -13,6 +15,14 @@ fi
 
 # create ~/.local/bin
 [ ! -d "$HOME/.local/bin" ] && mkdir -p "$HOME/.local/bin"
+
+
+# install delta
+if [ ! -f "$HOME/.local/bin/delta" ]; then
+    curl -LOJR "https://github.com/dandavison/delta/releases/download/$delta_release/delta-$delta_release-x86_64-unknown-linux-musl.tar.gz"
+    tar xf "delta-$delta_release-x86_64-unknown-linux-musl.tar.gz" -C "$HOME/.local/bin" "delta-$delta_release-x86_64-unknown-linux-musl/delta" --strip-components=1
+    rm "delta-$delta_release-x86_64-unknown-linux-musl.tar.gz"
+fi
 
 # create backup and link dotfiles
 for file in minirc.dfl userhome-hidden; do

5fecb03747b633fa3ad65c3085d50901d88be8e2
chprat <49040338+chprat@users.noreply.github.com>
2024-10-28 22:49:36 +0100

    nvim: Add lsp config with lua set up

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local on_attach = function(_, _)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+                vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            end
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+        end,
+    },
+}

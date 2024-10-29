e058b54c1904866b7bb2076a6a90f13c4a8744f8
chprat <49040338+chprat@users.noreply.github.com>
2024-10-29 17:01:00 +0100

    wezterm: Update mouse selection word boundaries

--- wezterm.lua
+++ wezterm.lua
@@ -4,4 +4,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Solarized (light) (terminal.sexy)'
 config.hide_tab_bar_if_only_one_tab = true
 
+-- configure word boundaries for mouse selection
+config.selection_word_boundary = ' \t\n{}[]()"\'`,:;│'
+
 return config

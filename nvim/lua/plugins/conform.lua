86e1d01db9f4efeafd469505cd017575c868f64d
chprat <49040338+chprat@users.noreply.github.com>
2024-11-04 20:35:44 +0100

    nvim: Add conform plugins

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        branch = "nvim-0.9",
+        config = function()
+            local conform = require("conform")
+            conform.setup({
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                },
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                },
+            })
+            vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+                conform.format({
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 1000,
+                })
+            end, { desc = "Format file or range (in visual mode)" })
+        end,
+    },
+    {
+        "zapling/mason-conform.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "stevearc/conform.nvim",
+        },
+        config = function()
+            require("mason-conform").setup({})
+        end,
+    },
+}

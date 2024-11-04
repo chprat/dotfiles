ec64fc05a4e0fb938de0a2cce1babfdbcd75cbe8
chprat <49040338+chprat@users.noreply.github.com>
2024-11-04 20:02:24 +0100

    nvim: Add nvim-treesitter plugin

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.3",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "lua", "vim", "vimdoc" },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+        })
+    end,
+}

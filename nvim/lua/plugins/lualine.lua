21e211dec812da5b570d3b5915fb4d01366c62c7
chprat <49040338+chprat@users.noreply.github.com>
2024-11-04 21:19:35 +0100

    nvim: Add lualine plugin

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status")
+        lualine.setup({
+            extensions = { "nvim-tree", "lazy" },
+            sections = {
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+                lualine_x = {
+                    {
+                        lazy_status.updates,
+                        cond = lazy_status.has_updates,
+                    },
+                    { "encoding" },
+                    { "fileformat" },
+                    { "filetype" },
+                },
+            },
+        })
+    end,
+}

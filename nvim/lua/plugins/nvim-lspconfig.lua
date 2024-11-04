009b885ba87b146e43e136cad74ef337fe649970
chprat <49040338+chprat@users.noreply.github.com>
2024-11-04 21:44:09 +0100

    nvim: Add nvim-cmp plugin

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,10 +15,12 @@ return {
         "neovim/nvim-lspconfig",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
             { "antosha417/nvim-lsp-file-operations", config = true },
         },
         config = function()
             local lspconfig = require("lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
             local keymap = vim.keymap
 
             vim.api.nvim_create_autocmd("LspAttach", {
@@ -70,7 +72,10 @@ return {
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
             end
 
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
             lspconfig.lua_ls.setup({
+                capabilities = capabilities,
                 settings = {
                     Lua = {
                         diagnostics = {

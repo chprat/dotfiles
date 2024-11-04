3d08ecde4a3dba3f98d6dad8cf99c70b487d84f5
chprat <49040338+chprat@users.noreply.github.com>
2024-11-04 19:59:00 +0100

    nvim: Add general settings and keymaps

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- split management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })

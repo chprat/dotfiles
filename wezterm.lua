8e5aed2157357ad5547e5075ad20d0cc6f0498e3
chprat <49040338+chprat@users.noreply.github.com>
2024-12-28 23:07:02 +0100

    wezterm: Set JetBrainsMono Nerd Font

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.font = wezterm.font("JetBrains Mono")
+
 config.color_scheme = "Solarized (light) (terminal.sexy)"
 config.hide_tab_bar_if_only_one_tab = true
 

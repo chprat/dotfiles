56f1c9c8eee095a59ac1a6e000b26141b2a6ed3b
chprat <49040338+chprat@users.noreply.github.com>
2025-01-11 12:00:15 +0100

    nvim/options: Highlight text on yanking

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -47,3 +47,12 @@ vim.opt.cursorline = true
 
 -- use system clipboard as default register
 vim.opt.clipboard:append("unnamedplus")
+
+-- Highlight text when yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight text when yanking",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})

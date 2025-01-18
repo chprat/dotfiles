e825bb1da3637290aac75fb4c32c622bda276a5f
chprat <49040338+chprat@users.noreply.github.com>
2025-01-18 18:59:58 +0100

    nvim: Add neogit plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,6 +1,18 @@
 return {
     {
-        "tpope/vim-fugitive",
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            local keymap = vim.keymap
+            local neogit = require("neogit")
+            neogit.setup({})
+            keymap.set("n", "<leader>gg", "<cmd> Neogit <cr>", { desc = "Neogit open" })
+            keymap.set("n", "<leader>gl", "<cmd> NeogitLogCurrent <cr>", { desc = "Neogit log current file" })
+        end,
     },
     {
         "lewis6991/gitsigns.nvim",

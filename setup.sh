e55cdd5c95ac4a0815aa47a9d284908d11bb386b
chprat <49040338+chprat@users.noreply.github.com>
2025-03-15 22:07:15 +0000

    setup: Install fonts only on desktops

--- setup.sh
+++ setup.sh
@@ -74,12 +74,14 @@ if [ -n "$USRSHELL" ] && [ "$USRSHELL" != "zsh" ]; then
 fi
 
 # install JetBrainsMono Nerd Font
-[ ! -d "$HOME/.local/share/fonts" ] && mkdir -p "$HOME/.local/share/fonts"
-if [ ! -f "$HOME/.local/share/fonts/JetBrainsMonoNerdFontMono-Regular.ttf" ]; then
-    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
-    unzip -o -j JetBrainsMono.zip "*.ttf" -d "$HOME/.local/share/fonts"
-    rm JetBrainsMono.zip
-    fc-cache -f
+if [ "$is_desktop" = 1 ]; then
+    [ ! -d "$HOME/.local/share/fonts" ] && mkdir -p "$HOME/.local/share/fonts"
+    if [ ! -f "$HOME/.local/share/fonts/JetBrainsMonoNerdFontMono-Regular.ttf" ]; then
+        curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+        unzip -o -j JetBrainsMono.zip "*.ttf" -d "$HOME/.local/share/fonts"
+        rm JetBrainsMono.zip
+        fc-cache -f
+    fi
 fi
 
 # install github packages

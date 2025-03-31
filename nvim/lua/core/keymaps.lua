bd70d3b68623c27312faaa1de48bc9c21abd1095
chprat <49040338+chprat@users.noreply.github.com>
2025-03-31 08:58:21 +0200

    nvim/keymaps: Fix duplicate keymap issue
    Executing LUA scripts and trouble shared the same keymap and so trouble
    wasn't usable anymore.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -19,9 +19,9 @@ keymap.set("n", "<leader>sm", "<C-w>_", { desc = "Maximize current split" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- LUA execution/source
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source current LUA file" })
-vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Execute current LUA line" })
-vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "Execute current LUA highligh" })
+vim.keymap.set("n", "<space><space>e", "<cmd>source %<CR>", { desc = "Source current LUA file" })
+vim.keymap.set("n", "<space>e", ":.lua<CR>", { desc = "Execute current LUA line" })
+vim.keymap.set("v", "<space>e", ":lua<CR>", { desc = "Execute current LUA highligh" })
 
 -- terminal
 keymap.set("n", "<leader>ts", function()

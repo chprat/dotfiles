9bebb3a56c09be4ebe9bf92bf54ce5ed3bca24f8
chprat <49040338+chprat@users.noreply.github.com>
2025-08-29 20:28:18 +0200

    wezterm: Add variable font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local on_mac = wezterm.target_triple == "aarch64-apple-darwin"
 
 config.font = wezterm.font("JetBrains Mono")
+config.font_size = on_mac and 14 or 12
 
 config.color_scheme = "Rosé Pine Moon (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true

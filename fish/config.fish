ca3d0c5c9c178d799e3587b1663cc4477074ebb7
chprat <49040338+chprat@users.noreply.github.com>
2025-09-02 22:36:52 +0200

    fish: Add homebrew configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     set -g fish_greeting
 end
+
+if string match -q Darwin $(uname)
+    set -x HOMEBREW_NO_ENV_HINTS 1
+    set -x HOMEBREW_NO_ANALYTICS 1
+    fish_add_path --path /opt/homebrew/bin/
+    fish_add_path --path /opt/homebrew/opt/gnu-sed/libexec/gnubin
+end

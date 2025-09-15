0292b415a829ccaa49cc1d9cc842ea842959532e
chprat <49040338+chprat@users.noreply.github.com>
2025-09-15 19:09:48 +0200

    fish: Use bat instead of cat

---
+++ fish/functions/cat.fish
@@ -0,0 +1,3 @@
+function cat --wraps=bat --wraps='bat --paging=never' --wraps='bat --paging=never --style=plain' --description 'alias cat bat --paging=never --style=plain'
+    bat --paging=never --style=plain $argv
+end

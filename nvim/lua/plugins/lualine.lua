724ab855908a736eb1838e9d998dae0df1c4e1bb
chprat <49040338+chprat@users.noreply.github.com>
2025-12-16 13:27:18 +0100

    nvim: Clean up lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,7 @@
 return {
     "nvim-lualine/lualine.nvim",
-    optional = true,
     opts = function(_, opts)
-        opts.options.component_separators = { left = "", right = "" }
-        opts.options.section_separators = { left = "", right = "" }
         opts.options.theme = "rose-pine-alt"
-        table.insert(opts.sections.lualine_c, 1, { "fileformat" })
-        table.insert(opts.sections.lualine_c, 2, { "encoding" })
         opts.sections.lualine_z = {}
     end,
 }

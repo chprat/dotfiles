64d7a55001ca894d849f1b743e21a58090f06353
chprat <49040338+chprat@users.noreply.github.com>
2026-03-09 09:41:13 +0100

    wezterm: Remove title bar

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,9 +9,6 @@ config.font_size = on_mac and 14 or 12
 config.color_scheme = "Rosé Pine Moon (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
-config.integrated_title_button_style = "Gnome"
-
 wezterm.on("gui-startup", function()
     local _, _, window = mux.spawn_window({})
     window:gui_window():maximize()
